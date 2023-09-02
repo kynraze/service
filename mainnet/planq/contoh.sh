5db399cee5ae136ebbdcda67173a7870013ec7da
Kynraze <107695211+kynraze@users.noreply.github.com>
2023-09-02 15:44:19 +0700

    Create contoh.sh

---
+++ mainnet/planq/contoh.sh
@@ -0,0 +1,9 @@
+install_node() {
+    echo "Installing Node for Planq..."
+    # Lakukan langkah-langkah instalasi yang diperlukan di sini
+    # Contoh:
+    # sudo apt update
+    # sudo apt install -y package1 package2
+    # Konfigurasi lain yang diperlukan
+    echo "Node for Planq installed successfully."
+}
